b6f1f4b98e65756a6fab8fcfa0152cc9a8064a72
jos23 <antonyhumbergur123@gmail.com>
2025-05-29 16:00:15 +0300

    now

--- src/home/views.py
+++ src/home/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home (request):
+    return render(request, 'base.html')
+
+def products (request):
+    context = {
+        'products' : [
+            {'name' : 'Apple watch',
+             'price' : '299.99',
+             'desription' : 'Aluminium case, starlight sport' },
+
+            {'name' : 'iphone14',
+             'price' : '799.99',
+             'description' : '128GB, midnight color'}
+             ]
+    }
+    return render(request, 'home/products.html', context) 
